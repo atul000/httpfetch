34a3ea384441a9360fd0c58895d15c8750b8cbfb
atul <atulbeniwal15@gmail.com>
2019-10-28 15:29:49 +0530

    version 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -17,6 +22,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final String url = "https://swapi.co/api/people/";
+  List data;
+
+  @override
+  void initState() {
+    super.initState();
+    this.getJsonData();
+  }
+
+  Future<String> getJsonData() async {
+    var response = await http.get(
+        // encode the url
+        Uri.encodeFull(url),
+        // only accept json res
+        headers: {"Accept": "application/json"});
+    print(response.body);
+
+    setState(() {
+      var convertDataToJson = jsonDecode(response.body);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -24,12 +51,20 @@ class _HomePageState extends State<HomePage> {
         title: new Text("fetch data via http GET"),
       ),
       body: new ListView.builder(
-        itemCount: 0,
+        itemCount: 1,
         itemBuilder: (BuildContext context, int index) {
           return new Container(
             child: new Center(
               child: new Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  new Card(
+                    child: new Container(
+                      child: new Text("hello"),
+                      padding: const EdgeInsets.all(20.0),
+                    ),
+                  )
+                ],
               ),
             ),
           );
